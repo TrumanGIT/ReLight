737d1792bc637e0b864524a5a85f4bbe80168e96
Truman <saberxnekopokemoncheezer@gmail.com>
2025-12-06 21:32:58 -0600

    added in game editor, fade rgb and radius are now available to change in game.

--- include/Menu.h
+++ include/Menu.h
@@ -6,7 +6,6 @@ namespace UI {
     void Register();
     void __stdcall RenderSettings();
     void __stdcall RenderLightEditor();
-    void __stdcall RenderWindow();
     void saveSettingsToIni(); 
     void getAllLights(); 
     inline MENU_WINDOW reLightMenuWindow;
