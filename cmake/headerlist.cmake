874afea2b13c2477dba7830b590ed6507f60ffe3
Easychips <saberxnekopokemoncheezer@gmail.com>
2026-06-08 20:05:23 -0500

    include QTR Utilities

---
+++ cmake/headerlist.cmake
@@ -0,0 +1,18 @@
+set(headers ${headers}
+	include/CLibUtilsQTR/utils.hpp
+	include/CLibUtilsQTR/Animations.hpp
+	include/CLibUtilsQTR/BoundingBox.hpp
+	include/CLibUtilsQTR/DrawDebug.hpp
+	include/CLibUtilsQTR/FormReader.hpp
+	include/CLibUtilsQTR/Papyrus.hpp
+	include/CLibUtilsQTR/PresetSettings.hpp
+	include/CLibUtilsQTR/Serialization.hpp
+	include/CLibUtilsQTR/StringHelpers.hpp
+	include/CLibUtilsQTR/Tasker.hpp
+	include/CLibUtilsQTR/Ticker.hpp
+	include/CLibUtilsQTR/PresetHelpers/Config.hpp
+	include/CLibUtilsQTR/PresetHelpers/Getters.hpp
+	include/CLibUtilsQTR/PresetHelpers/PresetHelpers.hpp
+	include/CLibUtilsQTR/PresetHelpers/PresetHelpersTXT.hpp
+	include/CLibUtilsQTR/PresetHelpers/PresetHelpersYAML.hpp
+)
